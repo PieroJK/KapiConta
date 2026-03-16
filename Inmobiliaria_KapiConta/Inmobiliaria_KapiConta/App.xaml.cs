39a491bad023ddae68ca00f55e6dc46bfa2e9504
PieroJK <snow-7298@outlook.es>
2026-03-15 19:25:38 -0500

    Commint c#

---
+++ Inmobiliaria_KapiConta/Inmobiliaria_KapiConta/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Inmobiliaria_KapiConta
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
